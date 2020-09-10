f5e9a5d0d16b050609264d25eb4aa84e5551499d
Navya Kuchibhotla <kucnavya@amazon.com>
2020-09-10 10:09:59 -0700

    Refactore broker key store

---
+++ greengrass-mqtt-broker/src/main/java/com/aws/iot/evergreen/mqtt/broker/MQTTBrokerKeyStore.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.aws.iot.evergreen.mqtt.broker;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+
+public class MQTTBrokerKeyStore {
+
+    private static final int RSA_KEY_LENGTH = 2048;
+    private static final String RSA_KEY_INSTANCE = "RSA";
+    public static final String DEFAULT_KEYSTORE_PASSWORD_STRING = "";
+    public static final char[] DEFAULT_KEYSTORE_PASSWORD = DEFAULT_KEYSTORE_PASSWORD_STRING.toCharArray();
+
+    @Getter(AccessLevel.PACKAGE)
+    private static KeyStore brokerKeyStore;
+    private static KeyPair brokerKeyPair;
+
+    // getter
+    public static KeyPair getBrokerKeyPair() {
+        return brokerKeyPair;
+    }
+
+    /**
+     *
+     * @return broker key store
+     * @throws KeyStoreException exception in creating key store
+     */
+    public KeyStore getKeyStore() throws KeyStoreException {
+        try {
+            brokerKeyStore = loadBrokerKeyStore();
+        } catch (KeyStoreException | IOException | CertificateException | NoSuchAlgorithmException e) {
+            brokerKeyStore = createDefaultBrokerKeyStore();
+        }
+        return brokerKeyStore;
+    }
+
+    /**
+     *
+     * @param keyStore keystore to write to disk
+     * @throws IOException if there was an I/O problem with data
+     * @throws CertificateException  if any of the certificates included in the keystore data could not be stored
+     * @throws NoSuchAlgorithmException if the appropriate data integrity algorithm could not be found
+     * @throws KeyStoreException  if the keystore has not been initialized (loaded)
+     */
+    public void writeKeyStoreToDisk(KeyStore keyStore) throws
+        IOException, CertificateException, NoSuchAlgorithmException, KeyStoreException {
+        FileOutputStream outputStream = new FileOutputStream(getBrokerKeyStorePath());
+        keyStore.store(outputStream, DEFAULT_KEYSTORE_PASSWORD);
+        outputStream.flush();
+        outputStream.close();
+    }
+
+    /**
+     *
+     * @return path to broker key store
+     */
+    public String getBrokerKeyStorePath() {
+        return "work/" + MQTTService.SERVICE_NAME + "/serverstore.jks";
+    }
+
+    private KeyStore loadBrokerKeyStore() throws
+        KeyStoreException, IOException, CertificateException, NoSuchAlgorithmException {
+        FileInputStream is = new FileInputStream(getBrokerKeyStorePath());
+        KeyStore keystore = KeyStore.getInstance(KeyStore.getDefaultType());
+        keystore.load(is, DEFAULT_KEYSTORE_PASSWORD);
+        return keystore;
+    }
+
+    private KeyStore createDefaultBrokerKeyStore() throws KeyStoreException {
+        // Generate Broker keypair
+        try {
+            brokerKeyPair = newRSAKeyPair();
+        } catch (NoSuchAlgorithmException e) {
+            throw new KeyStoreException("unable to generate keypair for broker key store", e);
+        }
+
+        KeyStore ks = KeyStore.getInstance(KeyStore.getDefaultType());
+        try {
+            ks.load(null, DEFAULT_KEYSTORE_PASSWORD);
+        } catch (IOException | NoSuchAlgorithmException | CertificateException e) {
+            throw new KeyStoreException("unable to load broker keystore", e);
+        }
+        return ks;
+    }
+
+    private static KeyPair newRSAKeyPair() throws NoSuchAlgorithmException {
+        KeyPairGenerator kpg = KeyPairGenerator.getInstance(RSA_KEY_INSTANCE);
+        kpg.initialize(RSA_KEY_LENGTH);
+        return kpg.generateKeyPair();
+    }
+}
--- greengrass-mqtt-broker/src/main/java/com/aws/iot/evergreen/mqtt/broker/MQTTService.java
+++ greengrass-mqtt-broker/src/main/java/com/aws/iot/evergreen/mqtt/broker/MQTTService.java
@@ -1,42 +1,97 @@
-/* Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
- * SPDX-License-Identifier: Apache-2.0 */
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
 package com.aws.iot.evergreen.mqtt.broker;
 
+import com.aws.iot.evergreen.config.Topic;
+import com.aws.iot.evergreen.config.WhatHappened;
+import com.aws.iot.evergreen.dcm.certificate.CertificateManager;
+import com.aws.iot.evergreen.dcm.certificate.CertificateRequestGenerator;
 import com.aws.iot.evergreen.config.Topics;
+import com.aws.iot.evergreen.dcm.certificate.CsrProcessingException;
 import com.aws.iot.evergreen.dependency.ImplementsService;
 import com.aws.iot.evergreen.dependency.State;
 import com.aws.iot.evergreen.kernel.EvergreenService;
 
+import com.aws.iot.evergreen.kernel.Kernel;
+import com.aws.iot.evergreen.util.Coerce;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 import io.moquette.BrokerConstants;
 import io.moquette.broker.Server;
 import io.moquette.broker.config.IConfig;
 import io.moquette.broker.config.MemoryConfig;
 
 import javax.inject.Inject;
+
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.util.Map;
 import java.util.Properties;
+import org.bouncycastle.operator.OperatorCreationException;
 
 @ImplementsService(name = MQTTService.SERVICE_NAME, autostart = true)
 public class MQTTService extends EvergreenService {
     public static final String SERVICE_NAME = "aws.greengrass.mqtt";
+    public static final String DCM_SERVICE_NAME = "aws.greengrass.certificate.manager";
+    private static final JsonMapper OBJECT_MAPPER =
+        JsonMapper.builder().configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true).build();
+
+    // Moquette Properties
+    private static final String SSL_PORT = "8883";
+    private static final String ALLOW_ANONYMOUS = "true";
+    private static final String ENFORCE_CLIENT_AUTH = "true";
+
+    // Config Keys
+    private static final String CONFIG_TOPIC = "config";
+    private static final String CERTIFICATES_TOPIC = "certificates";
+    private static final String CLIENT_CERTS = "clients";
+    // TODO: Is this the right key?
+    private static final String RUNTIME_CONFIG_KEY = "runtime";
+
+    // Key store properties
+    private static final String DEFAULT_BROKER_CN = "greengrass-mqtt-broker";
+    private static final String BROKER_KEY_ALIAS = "MqttBroker";
+    private static final String KEYSTORE_PASSWORD_STRING = MQTTBrokerKeyStore.DEFAULT_KEYSTORE_PASSWORD_STRING;
+
+    private static MQTTBrokerKeyStore mqttBrokerKeyStore;
+    private static KeyStore brokerKeyStore;
     private final Server mqttBroker = new Server();
+    private final Kernel kernel;
+    private final CertificateManager certificateManager;
 
     /**
      * Constructor for EvergreenService.
      *
      * @param topics Root Configuration topic for this service
+     * @param kernel evergreen kernel
+     * @param certificateManager DCM's certificate manager
      */
     @Inject
-    public MQTTService(Topics topics) {
+    public MQTTService(Topics topics, Kernel kernel, CertificateManager certificateManager) {
         super(topics);
+        this.kernel = kernel;
+        this.certificateManager = certificateManager;
     }
 
     @Override
     public void startup() {
         try {
+            configureKeyStore();
             mqttBroker.startServer(getDefaultConfig());
-        } catch (IOException e) {
+        } catch (IOException | KeyStoreException | CsrProcessingException e) {
             serviceErrored(e);
             return;
         }
@@ -48,21 +103,112 @@ public class MQTTService extends EvergreenService {
         mqttBroker.stopServer();
     }
 
-    public IConfig getDefaultConfig() {
+    private void configureKeyStore() throws
+        KeyStoreException, CsrProcessingException {
+        // get basic keystore from KeyStore
+        mqttBrokerKeyStore = new MQTTBrokerKeyStore();
+        brokerKeyStore = mqttBrokerKeyStore.getKeyStore();
+
+        // Subscribe to broker cert updates from DCM
+        subscribeToBrokerCert();
+        // Subscribe to client cert updates from DCM
+        Topics dcmConfig = kernel.findServiceTopic(DCM_SERVICE_NAME);
+        if (dcmConfig != null) {
+            dcmConfig.lookup(RUNTIME_CONFIG_KEY, CONFIG_TOPIC, CERTIFICATES_TOPIC, CLIENT_CERTS)
+                .subscribe(this::onClientCertsChange);
+        }
+    }
+
+    private void subscribeToBrokerCert() throws KeyStoreException, CsrProcessingException {
+
+        String csr;
+        try {
+            csr = CertificateRequestGenerator.createCSR(MQTTBrokerKeyStore.getBrokerKeyPair(),
+                DEFAULT_BROKER_CN,
+                null,
+                null);
+        } catch (IOException | OperatorCreationException e) {
+            throw new KeyStoreException("unable to generate CSR from keypair", e);
+        }
+        certificateManager.subscribeToCertificateUpdates(csr, this::updateCertInKeyStore);
+    }
+
+    private void onClientCertsChange(WhatHappened whatHappened, Topic topic) {
+        Topic certs = this.config.lookup(RUNTIME_CONFIG_KEY,
+            CONFIG_TOPIC,
+            CERTIFICATES_TOPIC,
+            CLIENT_CERTS).dflt("[]");
+        Map<String, String> clientCerts;
+        String val = Coerce.toString(certs);
+
+        if (val != null) {
+            try {
+                clientCerts = OBJECT_MAPPER.readValue(val, new TypeReference<Map<String, String>>() { });
+                // import client certs to keystore
+                importClientCertsIntoKeyStore(clientCerts);
+            } catch (IOException | CertificateException | KeyStoreException | NoSuchAlgorithmException e) {
+                logger.atError().kv("node", certs.getFullName()).kv("value", val)
+                    .log("Malformed client certs", e);
+            }
+        }
+    }
+
+    private void updateCertInKeyStore(String certPem) {
+        try {
+            X509Certificate cert = pemToX509Certificate(certPem);
+            Certificate[] certChain = {cert};
+            brokerKeyStore.setKeyEntry(BROKER_KEY_ALIAS,
+                MQTTBrokerKeyStore.getBrokerKeyPair().getPrivate(),
+                MQTTBrokerKeyStore.DEFAULT_KEYSTORE_PASSWORD,
+                certChain);
+            // stop mqtt server, write keystore back to disk and restart server
+            restartMqttServer();
+        } catch (CertificateException | IOException | KeyStoreException | NoSuchAlgorithmException e) {
+            //consumer can only throw runtime exception
+            throw new RuntimeException("unable to store generated cert", e);
+        }
+    }
+
+    private KeyStore importClientCertsIntoKeyStore(Map<String, String> clientCerts)
+        throws IOException, CertificateException, KeyStoreException, NoSuchAlgorithmException {
+        // TODO: check if cert alias exists
+        for (Map.Entry<String, String> entry : clientCerts.entrySet()) {
+            X509Certificate cert = pemToX509Certificate(entry.getValue());
+            brokerKeyStore.setCertificateEntry(entry.getKey(), cert);
+        }
+        // stop mqtt server, write keystore back to disk and restart server
+        restartMqttServer();
+        return brokerKeyStore;
+    }
+
+    private void restartMqttServer() throws
+        IOException, CertificateException, NoSuchAlgorithmException, KeyStoreException {
+        mqttBroker.stopServer();
+        mqttBrokerKeyStore.writeKeyStoreToDisk(brokerKeyStore);
+        mqttBroker.startServer();
+    }
+
+    private IConfig getDefaultConfig() {
         // TODO: Enable SSL, get certs from DCM
         IConfig defaultConfig = new MemoryConfig(new Properties());
 
-        defaultConfig.setProperty(BrokerConstants.HOST_PROPERTY_NAME, "127.0.0.1");
-        defaultConfig.setProperty(BrokerConstants.SSL_PORT_PROPERTY_NAME, "8883");
-
-        defaultConfig.setProperty(BrokerConstants.JKS_PATH_PROPERTY_NAME, "serverstore.p12");
-        defaultConfig.setProperty(BrokerConstants.KEY_STORE_TYPE, "pkcs12");
-        defaultConfig.setProperty(BrokerConstants.KEY_STORE_PASSWORD_PROPERTY_NAME, "passw0rdsrv");
-        defaultConfig.setProperty(BrokerConstants.KEY_MANAGER_PASSWORD_PROPERTY_NAME, "passw0rdsrv");
-
-        defaultConfig.setProperty(BrokerConstants.ALLOW_ANONYMOUS_PROPERTY_NAME, "true");
-        defaultConfig.setProperty(BrokerConstants.NEED_CLIENT_AUTH, "true");
+        defaultConfig.setProperty(BrokerConstants.SSL_PORT_PROPERTY_NAME, SSL_PORT);
+        defaultConfig.setProperty(BrokerConstants.JKS_PATH_PROPERTY_NAME, mqttBrokerKeyStore.getBrokerKeyStorePath());
+        defaultConfig.setProperty(BrokerConstants.KEY_STORE_PASSWORD_PROPERTY_NAME, KEYSTORE_PASSWORD_STRING);
+        defaultConfig.setProperty(BrokerConstants.KEY_MANAGER_PASSWORD_PROPERTY_NAME, KEYSTORE_PASSWORD_STRING);
+        defaultConfig.setProperty(BrokerConstants.ALLOW_ANONYMOUS_PROPERTY_NAME, ALLOW_ANONYMOUS);
+        defaultConfig.setProperty(BrokerConstants.NEED_CLIENT_AUTH, ENFORCE_CLIENT_AUTH);
 
         return defaultConfig;
+    }
+
+    private X509Certificate pemToX509Certificate(String certPem) throws IOException, CertificateException {
+        byte[] certBytes = certPem.getBytes(StandardCharsets.UTF_8);
+        CertificateFactory certFactory = CertificateFactory.getInstance("X.509");
+        X509Certificate cert;
+        try (InputStream certStream = new ByteArrayInputStream(certBytes)) {
+            cert = (X509Certificate) certFactory.generateCertificate(certStream);
+        }
+        return cert;
     }
 }
