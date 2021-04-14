9ec96ddd89a70f1529e6666190a26e64dcdfaf96
Joe Butler <508579+jbutler@users.noreply.github.com>
2021-04-13 20:33:25 -0600

    feat: integrate with client device auth component (#32)
    This change implements authentication and authorization for
    connecting client devices. Client certificates are authenticated
    during TLS handshake in a customer TrustManager. This TrustManager
    creates a session with the Client Devices Auth component, which
    gets used in future authorization decisions (connect, publish, subscribe)

---
+++ greengrass-mqtt-broker/src/main/java/com/aws/greengrass/mqttbroker/ClientDeviceAuthorizer.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.aws.greengrass.mqttbroker;
+
+import com.aws.greengrass.device.AuthorizationRequest;
+import com.aws.greengrass.device.DeviceAuthClient;
+import com.aws.greengrass.device.exception.AuthorizationException;
+import com.aws.greengrass.logging.api.Logger;
+import com.aws.greengrass.logging.impl.LogManager;
+import io.moquette.broker.security.ClientData;
+import io.moquette.broker.security.IAuthenticator;
+import io.moquette.broker.security.IAuthorizatorPolicy;
+import io.moquette.broker.subscriptions.Topic;
+
+import java.security.cert.X509Certificate;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ClientDeviceAuthorizer implements IAuthenticator, IAuthorizatorPolicy {
+    private static final Logger LOG = LogManager.getLogger(ClientDeviceAuthorizer.class);
+    private static final String CLIENT_ID = "clientId";
+    private static final String SESSION_ID = "sessionId";
+
+    private final ClientDeviceTrustManager trustManager;
+    private final DeviceAuthClient deviceAuthClient;
+    private final Map<String, String> clientToSessionMap;
+
+    /**
+     * Constructor.
+     * @param trustManager Trust manager
+     * @param deviceAuthClient Device auth client
+     */
+    public ClientDeviceAuthorizer(ClientDeviceTrustManager trustManager, DeviceAuthClient deviceAuthClient) {
+        this.trustManager = trustManager;
+        this.deviceAuthClient = deviceAuthClient;
+        this.clientToSessionMap = new ConcurrentHashMap<>();
+    }
+
+    @Override
+    public boolean checkValid(ClientData clientData) {
+        if (!clientData.getCertificates().isPresent()) {
+            LOG.error("No certificate in client data");
+            return false;
+        }
+        X509Certificate[] certificateChain = (X509Certificate[]) clientData.getCertificates().get();
+
+        // Retrieve session ID and construct authorization request for MQTT CONNECT
+        String sessionId = trustManager.getSessionForCertificate(certificateChain);
+        String clientId = clientData.getClientId();
+        LOG.atInfo().kv(CLIENT_ID, clientId).kv(SESSION_ID, sessionId).log("Retrieved client session");
+
+        boolean canConnect = canDevicePerform(sessionId, clientId, "mqtt:connect", "mqtt:clientId:" + clientId);
+
+        // Add mapping from client id to session id for future canRead/canWrite calls
+        if (canConnect) {
+            LOG.atInfo().kv(CLIENT_ID, clientId).kv(SESSION_ID, sessionId)
+                .log("Successfully authenticated client device");
+            clientToSessionMap.put(clientId, sessionId);
+        } else {
+            // TODO: Need to clean up this session since the device will be disconnected
+            LOG.atInfo().kv(CLIENT_ID, clientId).kv(SESSION_ID, sessionId)
+                .log("Device not authorized to connect");
+        }
+
+        return canConnect;
+    }
+
+    @Override
+    public boolean canWrite(Topic topic, String user, String client) {
+        LOG.atDebug().kv("topic", topic).kv("user", user).kv(CLIENT_ID, client)
+            .log("MQTT publish request");
+        return canDevicePerform(client, "mqtt:publish", "mqtt:topic:" + topic);
+    }
+
+    @Override
+    public boolean canRead(Topic topic, String user, String client) {
+        LOG.atDebug().kv("topic", topic).kv("user", user).kv(CLIENT_ID, client)
+            .log("MQTT subscribe request");
+        return canDevicePerform(client, "mqtt:subscribe", "mqtt:topic:" + topic);
+    }
+
+    private boolean canDevicePerform(String client, String operation, String resource) {
+        String sessionId = getSessionForClientId(client);
+        if (sessionId == null) {
+            LOG.atError().kv(CLIENT_ID, client).kv("operation", operation).kv("resource", resource)
+                .log("Unknown client request, denying request");
+            return false;
+        }
+        return canDevicePerform(sessionId, client, operation, resource);
+    }
+
+    private boolean canDevicePerform(String session, String client, String operation, String resource) {
+        try {
+            AuthorizationRequest authorizationRequest = AuthorizationRequest.builder()
+                .sessionId(session)
+                .clientId(client)
+                .operation(operation)
+                .resource(resource)
+                .build();
+            return deviceAuthClient.canDevicePerform(authorizationRequest);
+        } catch (AuthorizationException e) {
+            LOG.atError().kv(SESSION_ID, session).cause(e)
+                .log("session ID is invalid");
+        }
+        return false;
+    }
+
+    protected String getSessionForClientId(String clientId) {
+        return clientToSessionMap.getOrDefault(clientId, null);
+    }
+}
---
+++ greengrass-mqtt-broker/src/test/java/com/aws/greengrass/mqttbroker/ClientDeviceAuthorizerTest.java
@@ -0,0 +1,191 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.aws.greengrass.mqttbroker;
+
+import com.aws.greengrass.device.AuthorizationRequest;
+import com.aws.greengrass.device.DeviceAuthClient;
+import com.aws.greengrass.device.exception.AuthorizationException;
+import com.aws.greengrass.testcommons.testutilities.GGExtension;
+import com.aws.greengrass.testcommons.testutilities.GGServiceTestUtil;
+import io.moquette.broker.security.ClientData;
+import io.moquette.broker.subscriptions.Topic;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.security.cert.X509Certificate;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith({MockitoExtension.class, GGExtension.class})
+public class ClientDeviceAuthorizerTest extends GGServiceTestUtil {
+    @Mock
+    ClientDeviceTrustManager mockTrustManager;
+
+    @Mock
+    DeviceAuthClient mockDeviceAuthClient;
+
+    @Mock
+    X509Certificate mockCertificate;
+
+    private static final String DEFAULT_SESSION = "SESSION_ID";
+    private static final String DEFAULT_CLIENT = "clientId";
+    private static final String DEFAULT_TOPIC = "topic";
+
+    void configureAuthResponse(String session, String clientId, String operation, String resource, boolean doAllow) throws AuthorizationException {
+        AuthorizationRequest authorizationRequest = AuthorizationRequest.builder()
+            .sessionId(session)
+            .clientId(clientId)
+            .operation(operation)
+            .resource(resource)
+            .build();
+        when(mockDeviceAuthClient.canDevicePerform(authorizationRequest))
+            .thenReturn(doAllow);
+    }
+
+    void configureConnectResponse(String session, String clientId, boolean doAllow) throws AuthorizationException {
+        configureAuthResponse(session, clientId, "mqtt:connect", "mqtt:clientId:" + clientId, doAllow);
+    }
+
+    void configureConnectResponse(boolean doAllow) throws AuthorizationException {
+        configureConnectResponse(DEFAULT_SESSION, DEFAULT_CLIENT, doAllow);
+    }
+
+    void configurePublishResponse(String session, String clientId, String topic, boolean doAllow) throws AuthorizationException {
+        configureAuthResponse(session, clientId, "mqtt:publish", "mqtt:topic:" + topic, doAllow);
+    }
+
+    void configurePublishResponse(boolean doAllow) throws AuthorizationException {
+        configurePublishResponse(DEFAULT_SESSION, DEFAULT_CLIENT, DEFAULT_TOPIC, doAllow);
+    }
+
+    void configureSubscribeResponse(String session, String clientId, String topic, boolean doAllow) throws AuthorizationException {
+        configureAuthResponse(session, clientId, "mqtt:subscribe", "mqtt:topic:" + topic, doAllow);
+    }
+
+    void configureSubscribeResponse(boolean doAllow) throws AuthorizationException {
+        configureSubscribeResponse(DEFAULT_SESSION, DEFAULT_CLIENT, DEFAULT_TOPIC, doAllow);
+    }
+
+    @Test
+    void GIVEN_clientDataWithoutCertificate_WHEN_checkValid_THEN_returnsFalse() {
+        ClientDeviceAuthorizer authorizer = new ClientDeviceAuthorizer(mockTrustManager, mockDeviceAuthClient);
+        ClientData clientData = new ClientData(DEFAULT_CLIENT);
+        assertThat(authorizer.checkValid(clientData), is(false));
+    }
+
+    @Test
+    void GIVEN_unauthorizedClient_WHEN_checkValid_THEN_returnsFalse() throws AuthorizationException {
+        ClientDeviceAuthorizer authorizer = new ClientDeviceAuthorizer(mockTrustManager, mockDeviceAuthClient);
+        ClientData clientData = new ClientData(DEFAULT_CLIENT);
+        clientData.setCertificateChain(new X509Certificate[]{mockCertificate});
+
+        when(mockTrustManager.getSessionForCertificate(any())).thenReturn(DEFAULT_SESSION);
+        configureConnectResponse(false);
+
+        assertThat(authorizer.checkValid(clientData), is(false));
+    }
+
+    @Test
+    void GIVEN_authorizedClient_WHEN_checkValid_THEN_returnsTrue() throws AuthorizationException {
+        ClientDeviceAuthorizer authorizer = new ClientDeviceAuthorizer(mockTrustManager, mockDeviceAuthClient);
+        ClientData clientData = new ClientData(DEFAULT_CLIENT);
+        clientData.setCertificateChain(new X509Certificate[]{mockCertificate});
+
+        when(mockTrustManager.getSessionForCertificate(any())).thenReturn(DEFAULT_SESSION);
+        configureConnectResponse(true);
+
+        assertThat(authorizer.checkValid(clientData), is(true));
+    }
+
+    @Test
+    void GIVEN_unknownClient_WHEN_canReadCanWrite_THEN_returnsFalse() {
+        ClientDeviceAuthorizer authorizer = new ClientDeviceAuthorizer(mockTrustManager, mockDeviceAuthClient);
+
+        assertThat(authorizer.canRead(Topic.asTopic(DEFAULT_TOPIC), "user", DEFAULT_CLIENT), is(false));
+        assertThat(authorizer.canWrite(Topic.asTopic(DEFAULT_TOPIC), "user", DEFAULT_CLIENT), is(false));
+    }
+
+    @Test
+    void GIVEN_unauthorizedClient_WHEN_canReadCanWrite_THEN_returnsFalse() throws AuthorizationException {
+        ClientDeviceAuthorizer authorizer = new ClientDeviceAuthorizer(mockTrustManager, mockDeviceAuthClient);
+        ClientData clientData = new ClientData(DEFAULT_CLIENT);
+        clientData.setCertificateChain(new X509Certificate[]{mockCertificate});
+
+        when(mockTrustManager.getSessionForCertificate(any())).thenReturn(DEFAULT_SESSION);
+        configureConnectResponse(true);
+        configureSubscribeResponse(false);
+        configurePublishResponse(false);
+
+        assertThat(authorizer.checkValid(clientData), is(true));
+        assertThat(authorizer.canRead(Topic.asTopic(DEFAULT_TOPIC), "user", DEFAULT_CLIENT), is(false));
+        assertThat(authorizer.canWrite(Topic.asTopic(DEFAULT_TOPIC), "user", DEFAULT_CLIENT), is(false));
+    }
+
+    @Test
+    void GIVEN_unauthorizedClient_WHEN_canReadCanWrite_THEN_returnsTrue() throws AuthorizationException {
+        ClientDeviceAuthorizer authorizer = new ClientDeviceAuthorizer(mockTrustManager, mockDeviceAuthClient);
+        ClientData clientData = new ClientData(DEFAULT_CLIENT);
+        clientData.setCertificateChain(new X509Certificate[]{mockCertificate});
+
+        when(mockTrustManager.getSessionForCertificate(any())).thenReturn(DEFAULT_SESSION);
+        configureConnectResponse(true);
+        configureSubscribeResponse(true);
+        configurePublishResponse(true);
+
+        assertThat(authorizer.checkValid(clientData), is(true));
+        assertThat(authorizer.canRead(Topic.asTopic(DEFAULT_TOPIC), "user", DEFAULT_CLIENT), is(true));
+        assertThat(authorizer.canWrite(Topic.asTopic(DEFAULT_TOPIC), "user", DEFAULT_CLIENT), is(true));
+    }
+
+    @Test
+    void GIVEN_twoClientsWithDifferingPermissions_WHEN_canReadCanWrite_THEN_correctSessionIsUsed() throws AuthorizationException {
+        ClientDeviceAuthorizer authorizer = new ClientDeviceAuthorizer(mockTrustManager, mockDeviceAuthClient);
+        String session1 = "SESSION_ID1";
+        String session2 = "SESSION_ID2";
+        String client1 = "clientId1";
+        String client2 = "clientId2";
+        String topic1 = "topic/client1";
+        String topic2 = "topic/client2";
+        X509Certificate[] chain1 = {mock(X509Certificate.class)};
+        X509Certificate[] chain2 = {mock(X509Certificate.class)};
+        ClientData clientData1 = new ClientData(client1);
+        ClientData clientData2 = new ClientData(client2);
+        clientData1.setCertificateChain(chain1);
+        clientData2.setCertificateChain(chain2);
+
+        // Client1 can connect and publish/subscribe on own topic, but not client2's topics
+        when(mockTrustManager.getSessionForCertificate(chain1)).thenReturn(session1);
+        configureConnectResponse(session1, client1, true);
+        configurePublishResponse(session1, client1, topic1, true);
+        configureSubscribeResponse(session1, client1, topic1, true);
+        configurePublishResponse(session1, client1, topic2, false);
+        configureSubscribeResponse(session1, client1, topic2, false);
+
+        // Client2 can connect and publish/subscribe on own topic, but not client1's topics
+        when(mockTrustManager.getSessionForCertificate(chain2)).thenReturn(session2);
+        configureConnectResponse(session2, client2, true);
+        configurePublishResponse(session2, client2, topic1, false);
+        configureSubscribeResponse(session2, client2, topic1, false);
+        configurePublishResponse(session2, client2, topic2, true);
+        configureSubscribeResponse(session2, client2, topic2, true);
+
+        assertThat(authorizer.checkValid(clientData1), is(true));
+        assertThat(authorizer.checkValid(clientData2), is(true));
+        assertThat(authorizer.canRead(Topic.asTopic(topic1), "", client1), is(true));
+        assertThat(authorizer.canWrite(Topic.asTopic(topic1), "", client1), is(true));
+        assertThat(authorizer.canRead(Topic.asTopic(topic2), "", client1), is(false));
+        assertThat(authorizer.canWrite(Topic.asTopic(topic2), "", client1), is(false));
+        assertThat(authorizer.canRead(Topic.asTopic(topic1), "", client2), is(false));
+        assertThat(authorizer.canWrite(Topic.asTopic(topic1), "", client2), is(false));
+        assertThat(authorizer.canRead(Topic.asTopic(topic2), "", client2), is(true));
+        assertThat(authorizer.canWrite(Topic.asTopic(topic2), "", client2), is(true));
+    }
+}
