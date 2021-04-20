8c2cb87eafa61d6a1e8a58e18694e77cbe7c45e0
Joe Butler <508579+jbutler@users.noreply.github.com>
2021-04-20 16:43:07 -0600

    fix: remove client device config updates (#36)

--- greengrass-mqtt-broker/src/test/java/com/aws/greengrass/mqttbroker/ClientDeviceTrustManagerTest.java
+++ greengrass-mqtt-broker/src/test/java/com/aws/greengrass/mqttbroker/ClientDeviceTrustManagerTest.java
@@ -21,7 +21,7 @@ import java.security.cert.X509Certificate;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.when;
@@ -46,7 +46,7 @@ public class ClientDeviceTrustManagerTest extends GGServiceTestUtil {
     @Test
     void GIVEN_encodableCertificate_WHEN_checkClientTrust_THEN_noExceptionThrown() throws CertificateException {
         when(mockCertificate.getEncoded()).thenReturn(new byte[]{0});
-        when(mockDeviceAuthClient.createSession(any())).thenReturn("session_id");
+        when(mockDeviceAuthClient.createSession(anyString())).thenReturn("session_id");
         ClientDeviceTrustManager trustManager = new ClientDeviceTrustManager(mockDeviceAuthClient);
         trustManager.checkClientTrusted(new X509Certificate[]{mockCertificate}, "RSA");
     }
@@ -54,7 +54,7 @@ public class ClientDeviceTrustManagerTest extends GGServiceTestUtil {
     @Test
     void GIVEN_unauthenticatedCertificate_WHEN_checkClientTrust_THEN_CertificateExceptionThrown() throws CertificateEncodingException {
         when(mockCertificate.getEncoded()).thenReturn(new byte[]{0});
-        when(mockDeviceAuthClient.createSession(any())).thenReturn(null);
+        when(mockDeviceAuthClient.createSession(anyString())).thenReturn(null);
         ClientDeviceTrustManager trustManager = new ClientDeviceTrustManager(mockDeviceAuthClient);
         Assertions.assertThrows(CertificateException.class,
             () -> trustManager.checkClientTrusted(new X509Certificate[]{mockCertificate}, "RSA"));
@@ -63,37 +63,37 @@ public class ClientDeviceTrustManagerTest extends GGServiceTestUtil {
     @Test
     void GIVEN_singleConnection_WHEN_getSessionForCertificate_THEN_sessionCreatedAndReturned() throws CertificateException {
         when(mockCertificate.getEncoded()).thenReturn(new byte[]{0});
-        when(mockDeviceAuthClient.createSession(any())).thenReturn("SESSION-ID");
+        when(mockDeviceAuthClient.createSession(anyString())).thenReturn("SESSION-ID");
         ClientDeviceTrustManager trustManager = new ClientDeviceTrustManager(mockDeviceAuthClient);
 
         // Session should be created when checking if certificate is trusted
         trustManager.checkClientTrusted(new X509Certificate[]{mockCertificate}, "RSA");
-        verify(mockDeviceAuthClient, times(1)).createSession(any());
+        verify(mockDeviceAuthClient, times(1)).createSession(anyString());
 
         // Session should be retrievable without creating additional session
         reset(mockDeviceAuthClient);
         assertThat(trustManager.getSessionForCertificate(new X509Certificate[]{mockCertificate}), is("SESSION-ID"));
-        verify(mockDeviceAuthClient, times(0)).createSession(any());
+        verify(mockDeviceAuthClient, times(0)).createSession(anyString());
     }
 
     @Test
     void GIVEN_twoConnectionsWithSameCert_WHEN_getSessionForCertificate_THEN_sessionIsCreatedOnDemand() throws CertificateException {
         when(mockCertificate.getEncoded()).thenReturn(new byte[]{0});
-        when(mockDeviceAuthClient.createSession(any())).thenReturn("SESSION-ID");
+        when(mockDeviceAuthClient.createSession(anyString())).thenReturn("SESSION-ID");
 
         // Two calls to checkClientTrusted should only result in 1 session being created
         ClientDeviceTrustManager trustManager = new ClientDeviceTrustManager(mockDeviceAuthClient);
         trustManager.checkClientTrusted(new X509Certificate[]{mockCertificate}, "RSA");
         trustManager.checkClientTrusted(new X509Certificate[]{mockCertificate}, "RSA");
-        verify(mockDeviceAuthClient, times(1)).createSession(any());
+        verify(mockDeviceAuthClient, times(1)).createSession(anyString());
 
         // Reset mock to return new session ID
-        when(mockDeviceAuthClient.createSession(any())).thenReturn("SESSION-ID2");
+        when(mockDeviceAuthClient.createSession(anyString())).thenReturn("SESSION-ID2");
 
         // A second session should be created after the first is retrieved
         assertThat(trustManager.getSessionForCertificate(new X509Certificate[]{mockCertificate}), is("SESSION-ID"));
         assertThat(trustManager.getSessionForCertificate(new X509Certificate[]{mockCertificate}), is("SESSION-ID2"));
-        verify(mockDeviceAuthClient, times(2)).createSession(any());
+        verify(mockDeviceAuthClient, times(2)).createSession(anyString());
     }
 
     @Test
@@ -104,11 +104,11 @@ public class ClientDeviceTrustManagerTest extends GGServiceTestUtil {
 
         // Two connections with different certificates should result in two sessions
         ClientDeviceTrustManager trustManager = new ClientDeviceTrustManager(mockDeviceAuthClient);
-        when(mockDeviceAuthClient.createSession(any())).thenReturn("SESSION-ID");
+        when(mockDeviceAuthClient.createSession(anyString())).thenReturn("SESSION-ID");
         trustManager.checkClientTrusted(new X509Certificate[]{mockCertificate}, "RSA");
-        when(mockDeviceAuthClient.createSession(any())).thenReturn("SESSION-ID2");
+        when(mockDeviceAuthClient.createSession(anyString())).thenReturn("SESSION-ID2");
         trustManager.checkClientTrusted(new X509Certificate[]{mockCertificate2}, "RSA");
-        verify(mockDeviceAuthClient, times(2)).createSession(any());
+        verify(mockDeviceAuthClient, times(2)).createSession(anyString());
 
         assertThat(trustManager.getSessionForCertificate(new X509Certificate[]{mockCertificate}), is("SESSION-ID"));
         assertThat(trustManager.getSessionForCertificate(new X509Certificate[]{mockCertificate2}), is("SESSION-ID2"));
