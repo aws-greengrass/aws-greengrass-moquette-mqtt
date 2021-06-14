fc2b88964330c981c4acdd1c1d906ffec618dcce
Joe Butler <508579+jbutler@users.noreply.github.com>
2021-06-13 22:34:44 -0600

    feat: add configuration options for host, port, and throttle rate

--- greengrass-mqtt-broker/src/test/java/com/aws/greengrass/mqttbroker/MQTTServiceTest.java
+++ greengrass-mqtt-broker/src/test/java/com/aws/greengrass/mqttbroker/MQTTServiceTest.java
@@ -11,7 +11,6 @@ import com.aws.greengrass.lifecyclemanager.GreengrassService;
 import com.aws.greengrass.lifecyclemanager.Kernel;
 import com.aws.greengrass.testcommons.testutilities.GGExtension;
 import com.aws.greengrass.testcommons.testutilities.GGServiceTestUtil;
-import io.moquette.BrokerConstants;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -21,13 +20,13 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.io.IOException;
-import java.net.ConnectException;
 import java.net.Socket;
 import java.nio.file.Path;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith({MockitoExtension.class, GGExtension.class})
@@ -53,12 +52,9 @@ public class MQTTServiceTest extends GGServiceTestUtil {
         kernel.shutdown();
     }
 
-    @Test
-    void GIVEN_Greengrass_with_broker_WHEN_start_nucleus_THEN_broker_starts_on_port_8883()
-        throws InterruptedException, IOException {
+    void startNucleusWithConfig(String config) throws InterruptedException {
         CountDownLatch serviceRunning = new CountDownLatch(1);
-        kernel.parseArgs("-r", rootDir.toAbsolutePath().toString(), "-i",
-            getClass().getResource("config.yaml").toString());
+        kernel.parseArgs("-r", rootDir.toAbsolutePath().toString(), "-i", getClass().getResource(config).toString());
         kernel.getContext().addGlobalStateChangeListener((GreengrassService service, State was, State newState) -> {
             if (service.getName().equals(MQTTService.SERVICE_NAME) && service.getState().equals(State.RUNNING)) {
                 serviceRunning.countDown();
@@ -66,29 +62,30 @@ public class MQTTServiceTest extends GGServiceTestUtil {
         });
         kernel.launch();
         assertTrue(serviceRunning.await(TEST_TIME_OUT_SEC, TimeUnit.SECONDS));
+    }
 
-        // Connect to port 8883 just to confirm server is listening on port
-        try (Socket socket = new Socket("localhost", 8883)) {
-            // empty
+    boolean isListeningOnPort(int port) {
+        try (Socket socket = new Socket("localhost", port)) {
+            return true;
+        } catch (IOException e) {
+            return false;
         }
     }
 
     @Test
-    void GIVEN_Greengrass_with_broker_WHEN_start_nucleus_THEN_broker_doesnt_start_on_plain_tcp_port()
-        throws InterruptedException {
-        CountDownLatch serviceRunning = new CountDownLatch(1);
-        kernel.parseArgs("-r", rootDir.toAbsolutePath().toString(), "-i",
-            getClass().getResource("config.yaml").toString());
-        kernel.getContext().addGlobalStateChangeListener((GreengrassService service, State was, State newState) -> {
-            if (service.getName().equals(MQTTService.SERVICE_NAME) && service.getState().equals(State.RUNNING)) {
-                serviceRunning.countDown();
-            }
-        });
-        kernel.launch();
-        assertTrue(serviceRunning.await(TEST_TIME_OUT_SEC, TimeUnit.SECONDS));
+    void GIVEN_defaultConfig_WHEN_startComponent_THEN_brokerStartsOnPort8883() throws InterruptedException {
+        startNucleusWithConfig("config.yaml");
 
-        assertThrows(ConnectException.class, () -> {
-            new Socket("localhost", BrokerConstants.PORT);
-        });
+        assertThat(isListeningOnPort(8883), is(true));
+        assertThat(isListeningOnPort(1883), is(false));
+    }
+
+    @Test
+    void GIVEN_nonDefaultPort_WHEN_startComponent_THEN_brokerStartsOnConfiguredPort() throws InterruptedException {
+        startNucleusWithConfig("nonDefaultPort.yaml");
+
+        assertThat(isListeningOnPort(9000), is(true));
+        assertThat(isListeningOnPort(8883), is(false));
+        assertThat(isListeningOnPort(1883), is(false));
     }
 }
