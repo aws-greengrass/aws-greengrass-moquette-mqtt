2a3a1a6b56bd09c07dfb6d3cd45f1a29e83697f3
Joe Butler <508579+jbutler@users.noreply.github.com>
2021-06-24 14:45:42 -0600

    chore: clean unused artifacts

--- local_install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-
-cp recipe.yaml target/recipe/aws.greengrass.mqtt-0.0.0.yaml
-cp greengrass-mqtt-broker/build/libs/aws.greengrass.mqtt.broker-0.12.1-fat-jar.jar target/artifacts/aws.greengrass.mqtt/0.0.0/aws.greengrass.mqtt.jar
-greengrass-cli component update --recipeDir target/recipe --artifactDir target/artifacts --merge "aws.greengrass.mqtt=0.0.0"
